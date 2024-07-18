6edb3b0fead1520831a2efb5dd030f7c8d37415e
mirjonyoldoshev <mirjonyoldashev>
2024-07-18 19:38:00 +0600

    first---

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import  { routers, route, navigation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashbord from './components/Dashbord';
+
+const App = () => {
+    return {
+        <Routers>
+           <Route path=>
+        </Routers>
+    }
+}
